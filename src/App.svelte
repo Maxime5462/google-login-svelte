b48f012be689c59d7af66b67275f97d6f0944535
Max <maxime.leszczynski@laposte.net>
2020-03-27 17:59:32 +0100

    Mise en place du Login Google

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import GoogleSignInButton from "./GoogleSignInButton.svelte";
+  import Logout from "./Logout.svelte";
+  let name = "world";
+
+</script>
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
+
+<GoogleSignInButton />
+
+<Logout />
